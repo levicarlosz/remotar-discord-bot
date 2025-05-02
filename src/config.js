2e315957f1ea8c4c1a992097cf686f4858cfea19
Levi Carlos <olevicarlos.dev@gmail.com>
2025-05-02 17:45:47 -0300

    corrigindo erro na verificacao das variaveis de ambiente

--- src/config.js
+++ src/config.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 
-if (!DISCORD_TOKEN || !CHANNEL_ID || !REMOTAR_URL || !DATE) {
-  throw new Error("Uma ou mais variáveis de ambiente estão faltando.");
-}
+const DISCORD_TOKEN = process.env.DISCORD_API_KEY;
+const CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
+const REMOTAR_URL = process.env.REMOTAR_URL;
+const DATE = process.env.DATE;
 
+if (!DISCORD_TOKEN || !CHANNEL_ID || !REMOTAR_URL || !DATE) {
+  throw new Error(
+    "Uma ou mais variáveis de ambiente estão faltando. Verifique o arquivo .env"
+  );
+}
 
 module.exports = {
   DISCORD_TOKEN: process.env.DISCORD_API_KEY,
